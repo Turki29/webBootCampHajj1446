fc5bb7dc43d4cb60e57fb6f4554a488991d289d9
Turki <TurkiMA1445@outlook.com>
2025-06-11 09:49:34 +0300

    البداية في جافاسكربت

---
+++ مقدمة JS/script.js
@@ -0,0 +1,13 @@
+let body = document.body;
+
+
+let div = document.createElement("h1");
+div.innerHTML = "<h1>خالد</h1>";
+console.log(div.innerHTML);
+
+body.innerHTML = body.innerHTML + div.innerHTML;
+
+
+
+
+
